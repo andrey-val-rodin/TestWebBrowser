636235574125c5661dfba22fecff87b305ad9ca7
Andrey Rodin <andrey.val.rodin@gmail.com>
2025-05-14 19:09:18 +0300

    Initial commit

---
+++ TestWebBrowser/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace TestWebBrowser
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (!File.Exists("index.html"))
+                MessageBox.Show("Файл index.html не найден");
+
+            var path = Path.GetFullPath("index.html").Replace("\\", "/");
+            MyWebBrowser.Source = new Uri($"file:///{path}");
+            MyWebBrowser.ObjectForScripting = new ScriptManager();
+        }
+    }
+}
---
+++ TestWebBrowser/ScriptManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace TestWebBrowser
+{
+    [ComVisible(true)]
+    public class ScriptManager
+    {
+        public void Notify(string text)
+        {
+            MessageBox.Show("Получено из JavaScript: " + text);
+        }
+    }
+}
